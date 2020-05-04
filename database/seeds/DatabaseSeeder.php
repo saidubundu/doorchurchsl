2785a7e95513342ed3cd732c1c84ac1858f47409
saidubundu <34603026+saidubundu@users.noreply.github.com>
2020-05-04 07:29:30 +0000

    Second Release

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -19,9 +19,9 @@ class DatabaseSeeder extends Seeder
 //        $this->call(PhotoTableSeeder::class);
 //        $this->call(VideoTableSeeder::class);
 //        $this->call(BibleTableSeeder::class);
-        $this->call(RolesTableSeeder::class);
-//        $this->call(CommentsTableSeeder::class);
-        $this->call(PermissionsTableSeeder::class);
+//        $this->call(RolesTableSeeder::class);
+////        $this->call(CommentsTableSeeder::class);
+//        $this->call(PermissionsTableSeeder::class);
 //        $this->call(CausesTableSeeder::class);
 //        $this->call(TestimoniesTableSeeder::class);
 
