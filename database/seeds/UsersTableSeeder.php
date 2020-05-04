c0a2064e84bfbde7679f2e01996980641d90bd61
saidubundu <34603026+saidubundu@users.noreply.github.com>
2020-05-04 07:45:17 +0000

    Second Release

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -15,27 +15,39 @@ class UsersTableSeeder extends Seeder
         //
 
         DB::table('users')->delete();
-        DB::table('users')->insert([
-            [
-                'name' => 'Door Church',
-                'email' => 'door@peektower.com',
-                'password' => bcrypt('password'),
-                'bio' => 'This is the bio'
-            ],
+        if (env('APP_ENV') === 'local') {
+            DB::table('users')->insert([
+                [
+                    'name' => 'Door Church',
+                    'email' => 'door@peektower.com',
+                    'password' => bcrypt('password'),
+                    'bio' => 'This is the bio'
+                ],
 
-            [
-                'name' => 'John Doe',
-                'email' => 'doe@peektower.com',
-                'password' => bcrypt('password'),
-                'bio' => 'This is the bio'
-            ],
+                [
+                    'name' => 'John Doe',
+                    'email' => 'doe@peektower.com',
+                    'password' => bcrypt('password'),
+                    'bio' => 'This is the bio'
+                ],
 
-            [
-                'name' => 'Jane Doe',
-                'email' => 'jane@peektower.com',
-                'password' => bcrypt('password'),
-                'bio' => 'This is the bio'
-            ],
-        ]);
+                [
+                    'name' => 'Jane Doe',
+                    'email' => 'jane@peektower.com',
+                    'password' => bcrypt('password'),
+                    'bio' => 'This is the bio'
+                ],
+            ]);
+        }else
+        {
+            DB::table('users')->insert([
+                [
+                    'name' => "Administrator",
+                    'email' => "admin@test.com",
+                    'password' => bcrypt('admin'),
+                    'bio' => "I'm an Administrator"
+                ]
+            ]);
+        }
     }
 }
